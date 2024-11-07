2893a8a9d5f09e3845d04fe738a382548ea7c6d5
rahma <fijriatirahmaturrizqi@gmail.com>
2024-11-07 08:22:30 +0700

    modifikasi percobaan 2 menambah menu, dan validasi baris kolom

--- BioskopWithScanner10.java
+++ BioskopWithScanner10.java
@@ -7,19 +7,68 @@ public class BioskopWithScanner10 {
         String[][] penonton = new String[4][2];
 
         while (true) {
-            System.out.print("masukkan nama: ");
-            String nama = sc.nextLine();
-            System.out.print("masukkan baris: ");
-            int baris = sc.nextInt();
-            System.out.print("masukkan kolom: ");
-            int kolom = sc.nextInt();
+            System.out.println("menu:");
+            System.out.println("1. input data penonton");
+            System.out.println("2. tampilkan data penonton");
+            System.out.println("3. keluar");
+            System.out.print("masukkan pilihan: ");
+
+            int pilihan = sc.nextInt();
             sc.nextLine();
+            switch (pilihan) {
+                case 1:
+                    while (true) {
+                        System.out.print("masukkan nama: ");
+                        String nama = sc.nextLine();
+                        System.out.print("masukkan baris: ");
+                        int baris = sc.nextInt();
+                        System.out.print("masukkan kolom: ");
+                        int kolom = sc.nextInt();
+                        sc.nextLine();
 
-            penonton[baris - 1][kolom - 1] = nama;
+                        if ((baris - 1) >= penonton.length || (kolom - 1) >= penonton[0].length) {
+                            System.out.println("baris / kolom tidak valid");
+                            continue;
+                        } else if (penonton[baris - 1][kolom - 1] != null) {
+                            System.out.println("baris / kolom sudah terisi");
+                            continue;
+                        } else {
+                            penonton[baris - 1][kolom - 1] = nama;
+                        }
 
-            System.out.print("input penonton lainnya? (y/n): ");
-            String next = sc.nextLine();
-            if (next.equalsIgnoreCase("n")) {
+                        System.out.print("input penonton lainnya? (y/n): ");
+                        String next = sc.nextLine();
+                        if (next.equalsIgnoreCase("y")) {
+                            continue;
+                        } else if (next.equalsIgnoreCase("n")) {
+                            break;
+                        } else {
+                            System.out.println("pilihan tidak valid");
+                            break;
+                        }
+                    }
+                    break;
+                case 2:
+                    for (int i = 0; i < penonton.length; i++) {
+                        for (int j = 0; j < penonton[0].length; j++) {
+                            if (penonton[i][j] == null) {
+                                System.out.print("\t***");
+                            } else {
+                                System.out.print("\t" + penonton[i][j]);
+                            }
+                        }
+                        System.out.println();
+                    }
+                    break;
+                case 3:
+                    System.out.println("terima kasih");
+                    break;
+                default:
+                    System.out.println("pilihan tidak valid");
+                    break;
+            }
+
+            if (pilihan == 3 || pilihan < 1 || pilihan > 3) {
                 break;
             }
         }
